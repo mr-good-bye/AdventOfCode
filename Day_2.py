1927e2054e6ba1db2f8be0ab04a1a9e61b81bdfd
mr-good-bye <nikitos99099@gmail.com>
2020-05-24 05:44:36 +0300

    Day 2 completed step 2

--- Day_2.py
+++ Day_2.py
@@ -1,15 +1,26 @@
-program = list(map(int, input().split(',')))
-i = 0
-program[1] = 12
-program[2] = 2
-while program[i] != 99:
-    if program[i] == 1:
-        program[program[i+3]] = program[program[i+1]] + program[program[i+2]]
-    elif program[i] == 2:
-        program[program[i+3]] = program[program[i+1]] * program[program[i+2]]
-    else:
-        print('Error, num is ' + program[i])
-    i = i + 4
+pr = list(map(int, input().split(',')))
+def calculate(n, v, program):
+    i = 0
+    program[1] = n
+    program[2] = v
+    while program[i] != 99:
+        if program[i] == 1:
+            program[program[i+3]] = program[program[i+1]] + program[program[i+2]]
+        elif program[i] == 2:
+            program[program[i+3]] = program[program[i+1]] * program[program[i+2]]
+        else:
+            return [0,0,0]
+        i = i + 4
+    return [program[0], n, v]
 
 
-print(program)
+def trying():
+    for n in range(181):
+        for v in range(181):
+            res = calculate(n, v, pr.copy())
+            if res[0] == 19690720:
+                print(res)
+                return 100*res[1]+res[2]
+
+
+print(trying())
